ec8d9becb20058d9fe9e2299e45ac112d5116ba1
Brandon Oxendine <brandon.oxendine@gmail.com>
2022-11-04 11:44:30 +0100

    add page title

---
+++ src/lib/components/pageTitle.svelte
@@ -0,0 +1,13 @@
+<a href="/" class="page-title"><h1><slot /></h1></a>
+
+<style>
+  .page-title {
+    width: 100%;
+    font-size: 6vw;
+    font-weight: normal;
+    font-family: var(--special-font);
+    top: var(--space-x-large);
+    position: fixed;
+    z-index: 1;
+  }
+</style>
